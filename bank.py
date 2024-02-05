f211ba2d2f01d85bcada56bb62b503e8f6dd2478
briannemcgrath <113979868+briannemcgrath@users.noreply.github.com>
2024-02-05 20:18:28 +0000

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,20 @@
+#bank.py
+#Prompt the user and read in two money amounts (in cent)
+#Add the two amounts 
+#Print out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount 
+#Author: Brianne McGrath 
+
+amount1 = int (input("Please enter the first amount (in cents):"))
+amount2 = int (input("Please enter the second amount (in cents):"))
+
+#creating the two input prompts
+
+floatnumber = amount1 + amount2
+
+#creating the sum of the two inputs
+
+answer = floatnumber / 100
+
+#making the answer into a demical format 
+
+print ("The sum of these is €",answer)
